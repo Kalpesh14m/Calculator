6ccf7d4d45def3f73972ff25b3b75889d4c3e88e
kalpesh-mali <kalpesh.mali@bridgelabz.com>
2020-10-06 10:07:18 +0530

    modified: revert changes

--- src/com/blz/calculator/runner/Calculator.java
+++ src/com/blz/calculator/runner/Calculator.java
@@ -3,14 +3,5 @@ package com.blz.calculator.runner;
 public class Calculator {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		Calculator calci = new Calculator();
-		int sum = calci.add(10, 20);
-		System.out.println("Addition: " + sum);
-	}
-
-	private int add(int numOne, int numTwo) {
-		int addition = 0;
-		addition = numOne + numTwo;
-		return addition;
 	}
 }
