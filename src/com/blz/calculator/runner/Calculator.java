e34ad12d14098325bc19dcfef760c615d9e51c13
kalpesh-mali <kalpesh.mali@bridgelabz.com>
2020-10-06 10:13:55 +0530

    Added: functionality sub added

--- src/com/blz/calculator/runner/Calculator.java
+++ src/com/blz/calculator/runner/Calculator.java
@@ -4,8 +4,15 @@ public class Calculator {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		Calculator calci = new Calculator();
+
+		// Add
 		int sum = calci.add(10, 20);
 		System.out.println("Addition: " + sum);
+
+		// Sub
+		int sub = calci.sub(10, 20);
+		System.out.println("Sub: " + sub);
+
 	}
 
 	private int add(int numOne, int numTwo) {
@@ -13,4 +20,11 @@ public class Calculator {
 		addition = numOne + numTwo;
 		return addition;
 	}
+
+	private int sub(int numOne, int numTwo) {
+		int sub = 0;
+		sub = numOne - numTwo;
+		return sub;
+	}
+
 }
